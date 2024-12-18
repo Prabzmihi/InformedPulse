4c218dbe46cd0929d4657f90e773e1feca74a756
Prabzmihi <145851333+Prabzmihi@users.noreply.github.com>
2024-12-18 19:27:04 +0200

    fixing path issue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import TopBar from '../../components/topbar/Topbar'
+import TopBar from '../../components/topbar/TopBar'
 import LeftBar from '../../components/leftbar/LeftBar'
 import Feed from '../../components/feed/Feed'
 import RightBar from '../../components/rightbar/RightBar'
